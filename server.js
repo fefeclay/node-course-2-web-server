da3b5ba6eb586ad54b63e6a37edcd4b953a98bd5
Felisha Clay <fefeclay@gmail.com>
2017-12-31 13:46:24 -0600

    Added new projects page

--- server.js
+++ server.js
@@ -46,13 +46,19 @@ app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
     });
-})
+});
 
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unanble to handle request'
     });
 });
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+    });
+})
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
